07c02d46cd7f7aac5d642febca54b9210ee80bc7
Bilol <madiyarovbilol78@gmail.com>
2025-07-21 20:37:06 +0500

    Api property added to books

--- src/book/dto/create-book.dto.ts
+++ src/book/dto/create-book.dto.ts
@@ -1,18 +1,24 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsBoolean, IsInt, IsString } from 'class-validator';
 
 export class CreateBookDto {
   @IsString()
+  @ApiProperty({example: 'title'})
   title: string;
 
   @IsString()
+  @ApiProperty({example: 'author'})
   author: string;
 
   @IsInt()
+  @ApiProperty({example: '1999'})
   publishedYear: number;
 
   @IsString()
+  @ApiProperty({example: 'fantastic'})
   genre: string;
 
   @IsBoolean()
+  @ApiProperty({example: false})
   isAvailable: boolean;
 }
