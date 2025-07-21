214bb6ec17286cf2f5e08ec69471df78f50eb918
Bilol <madiyarovbilol78@gmail.com>
2025-07-21 20:34:01 +0500

    api property added to user

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,9 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty } from 'class-validator';
 
 export class CreateUserDto {
   @IsNotEmpty()
+  @ApiProperty({ example: 'name' })
   name: string;
 
   @IsEmail()
+  @ApiProperty({ example: 'email@example.com' })
   email: string;
 }
